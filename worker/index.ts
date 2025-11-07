43b9fb0296709a4f5f03c7e40040b6ffca5b99bd
JasonCloudflare <jkuang-yao@cloudflare.com>
2025-11-08 00:02:44 +0800

    Update index.ts

--- worker/index.ts
+++ worker/index.ts
@@ -1,6 +1,9 @@
 import { Hono } from 'hono';
 import { Container, getContainer } from '@cloudflare/containers';
 const app = new Hono<{ Bindings: Env }>();
+interface Env {
+  command: KVNamespace;
+}
 
 export class SandboxShellContainer extends Container {
   defaultPort = 8000;
